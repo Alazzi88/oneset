658f5ef945d0ed3f37704b84a8a2e3d515658c8d
Yahya Alazzi <yahyaalazzi@Yahyas-MacBook-Air.local>
2025-05-31 16:13:37 +0300

    dd

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,7 +2,6 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Almarai } from 'next/font/google';
 
